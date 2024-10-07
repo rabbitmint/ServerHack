d44b0c16fec1a691d11915731fb85ab9bdeb660f
Administrator <vlrzmf915@gmail.com>
2024-10-07 17:27:46 +0900

    pre-alpha-FirstInvTest

--- src/main/java/com/rabbitmint/mainclass.java
+++ src/main/java/com/rabbitmint/mainclass.java
@@ -2,11 +2,16 @@ package com.rabbitmint;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 public final class mainclass extends JavaPlugin {
 
     @Override
     public void onEnable() {
         getLogger().info("합법핵 플러그인이 활성화 되었습니다.");
         getLogger().info("제작 : 민트토끼 , 아이디어 : EY");
+        getServer().getPluginManager().registerEvents(new CrouchAndSwapHandListener(), this);
     }
 }
